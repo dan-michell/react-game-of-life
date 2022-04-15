d20b00e63ff03558ab5d51976c43d5104b3dea0a
Dan Michell <dan.michell1000@gmail.com>
2022-04-15 23:31:29 +0100

    Created initial grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const grid = [];
+    for (let i = 0; i < 50; i++) {
+      const row = [];
+      for (let j = 0; j < 50; j++) {
+        row.push(0);
+      }
+      grid.push(row);
+    }
+    return grid;
+  });
+  console.log(grid);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{/* Display initial grid using a map and divs */}</div>
     </div>
   );
 }
