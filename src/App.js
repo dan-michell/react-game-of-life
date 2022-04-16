a5e223d6c362d98a681d6e13b96ef8982513b617
Dan Michell <dan.michell1000@gmail.com>
2022-04-16 22:50:06 +0100

    Basic game functionality implemented

--- src/App.js
+++ src/App.js
@@ -13,12 +13,66 @@ function App() {
     }
     return grid;
   });
+  const [running, setRunning] = useState(true);
 
   function addCell(e) {
     const newGrid = [...grid];
     const coords = e.target.id.split("-");
-    newGrid[coords[0]][coords[1]] = 1;
+    if (newGrid[coords[0]][coords[1]]) {
+      newGrid[coords[0]][coords[1]] = 0;
+    } else {
+      newGrid[coords[0]][coords[1]] = 1;
+    }
     setGrid(newGrid);
+  }
+
+  function timer(ms) {
+    return new Promise((res) => setTimeout(res, ms));
+  }
+
+  async function simulateGame() {
+    setRunning(!running);
+    const directions = [
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+      [-1, 0],
+      [1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+    ];
+
+    while (running) {
+      let newCellGrid = JSON.parse(JSON.stringify(grid));
+      console.log(running);
+      for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+          let numOfAlive = 0;
+          directions.forEach(([x, y]) => {
+            const newX = j + x;
+            const newY = i + y;
+
+            if (newX >= 0 && newY >= 0 && newX < grid[i].length && newY < grid.length) {
+              if (grid[newY][newX] === 1) numOfAlive += 1;
+            }
+          });
+          if (grid[i][j] === 1) {
+            if (numOfAlive < 2 || numOfAlive > 3) {
+              newCellGrid[i][j] = 0;
+            }
+          } else if (grid[i][j] === 0) {
+            if (numOfAlive === 3) {
+              newCellGrid[i][j] = 1;
+            }
+          }
+        }
+      }
+
+      await timer(1000);
+      const newGrid = [...newCellGrid];
+      setGrid(newGrid);
+    }
   }
 
   return (
@@ -26,7 +80,14 @@ function App() {
       <header>
         <h1>Game of Life</h1>
       </header>
-      <button className="start-button">Start</button>
+      <button
+        className="start-button"
+        onClick={(e) => {
+          simulateGame();
+        }}
+      >
+        {!running ? "Stop" : "Run"}
+      </button>
       <div className="grid-wrapper">
         {grid.map((row, y) =>
           row.map((cell, x) => {
@@ -38,7 +99,7 @@ function App() {
                   addCell(e);
                 }}
                 className="cell"
-                style={{ backgroundColor: grid[y][x] ? "black" : "white" }}
+                style={{ backgroundColor: grid[y][x] ? "#1cdd1a" : "white" }}
               ></div>
             );
           })
