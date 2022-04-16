89a763988bcf9bb6c436616368db83828f39121d
Dan Michell <dan.michell1000@gmail.com>
2022-04-16 02:16:47 +0100

    Added cell selection functionality

--- src/App.js
+++ src/App.js
@@ -13,10 +13,37 @@ function App() {
     }
     return grid;
   });
-  console.log(grid);
+
+  function addCell(e) {
+    const newGrid = [...grid];
+    const coords = e.target.id.split("-");
+    newGrid[coords[0]][coords[1]] = 1;
+    setGrid(newGrid);
+  }
+
   return (
     <div className="App">
-      <div>{/* Display initial grid using a map and divs */}</div>
+      <header>
+        <h1>Game of Life</h1>
+      </header>
+      <button className="start-button">Start</button>
+      <div className="grid-wrapper">
+        {grid.map((row, y) =>
+          row.map((cell, x) => {
+            return (
+              <div
+                key={`${y}-${x}`}
+                id={`${y}-${x}`}
+                onClick={(e) => {
+                  addCell(e);
+                }}
+                className="cell"
+                style={{ backgroundColor: grid[y][x] ? "black" : "white" }}
+              ></div>
+            );
+          })
+        )}
+      </div>
     </div>
   );
 }
